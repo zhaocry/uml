68020d93e89f505439674ef123ab43493ee1523c
zhaocry <zhaocry@gmail.com>
2020-02-22 00:38:39 +0800

    Update install.sh

--- lkl/install.sh
+++ lkl/install.sh
@@ -131,7 +131,7 @@ ip link set lkl-tap up
 sysctl -w net.ipv4.ip_forward=1
 iptables -P FORWARD ACCEPT 
 iptables -t nat -A POSTROUTING -o venet0 -j MASQUERADE
-iptables -t nat -A PREROUTING -i venet0 -p tcp -m multiport --dports 80,443,10001,18081 -j DNAT --to-destination 10.0.0.2
+iptables -t nat -A PREROUTING -i venet0 -p tcp --match multiport --dports 80,443,10001,18081 -j DNAT --to-destination 10.0.0.2
 
 nohup /root/lkl/lkl.sh &
 
